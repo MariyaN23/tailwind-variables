6ee8140a5cb8e7a7dec676a30fae1f0155bde18c
MariyaN23 <138151774+MariyaN23@users.noreply.github.com>
2025-10-08 16:57:17 +0300

    primevue

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './main.css'
+import 'primeicons/primeicons.css'
+import PrimeVue from 'primevue/config';
+import Lara from '@primeuix/themes/lara';
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(PrimeVue, {
+    theme: {
+        preset: Lara,
+        options: {
+            darkModeSelector: '.my-app-dark',
+        }
+    }
+})
+app.mount('#app')
